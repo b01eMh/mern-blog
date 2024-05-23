955d5f5f546d6020af15619bb2ce1fbf7c3d2eed
b01eMh <bsimic24@gmail.com>
2024-05-23 13:25:51 +0200

    Error handling.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,29 @@
 import 'dotenv/config';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import NoteModel from './models/note';
 
 const app = express();
 
-app.get('/', async (req, res) => {
-  const notes = await NoteModel.find().exec();
-  res.status(200).json(notes);
+app.get('/', async (req, res, next) => {
+  try {
+    const notes = await NoteModel.find().exec();
+    res.status(200).json(notes);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// 404 endpoint
+app.use((req, res, next) => {
+  next(Error('Endpoint not found.'));
+});
+
+// Error handling
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+  console.error(error);
+  let errorMessage = 'An unknown error occurred.';
+  if (error instanceof Error) errorMessage = error.message;
+  res.status(500).json({ error: errorMessage });
 });
 
 export default app;
